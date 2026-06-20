44c6bfb8636e950a32e5b1396f03b5c2998ae6b7
IzaltinoDomingosDeSouza <izaltinodsouza@gmail.com>
2026-06-19 21:15:52 -0300

    fix api call

--- src/renderer.h
+++ src/renderer.h
@@ -17,7 +17,7 @@ class Renderer
 public:
     virtual ~Renderer() = default;
     virtual void draw_texture(TextureHandle handle, float x, float y) = 0;
-    virtual void draw_texture(TextureHandle handle, float x, float y, float angle, Flip flip) = 0;
+    virtual void draw_texture(TextureHandle handle, float x, float y, float angle = 0.0f, Flip flip = Flip::None) = 0;
     virtual 	void draw_texture(TextureHandle handle, float x, float y, float origin_x, float origin_y, float angle, Flip flip = Flip::None) = 0;
     virtual void draw_texture(TextureHandle handle, FloatRect destination) = 0;
     virtual void draw_texture(TextureHandle handle, FloatRect source, FloatRect destination) = 0;
