ddc406163fa97c598a9a38e9ca3a1456a973cfca
IzaltinoDomingosDeSouza <izaltinodsouza@gmail.com>
2026-06-19 21:06:16 -0300

    make player move up and down

--- src/game.cpp
+++ src/game.cpp
@@ -4,6 +4,8 @@
 #include "sdl/application.h"
 #include "game_application.h"
 
+struct Vec2f { float x; float y; };
+
 class CometStrike : public GameApplication
 {
 public:
@@ -13,15 +15,26 @@ public:
 		
 		background_texture = resource_manager->import_texture("assets/background.png");
 		player_texture = resource_manager->import_texture("assets/spaceship.png");
+		
+		position = Vec2f{0,0};
 	}
 	void update(float delta_time) override
 	{
+		const bool * key_state = SDL_GetKeyboardState(NULL);
 		
+		if(key_state[SDL_SCANCODE_W])
+		{
+			position.y -= speed * delta_time;
+		}
+		if(key_state[SDL_SCANCODE_S])
+		{
+			position.y += speed * delta_time;
+		}
 	}
 	void render() override
 	{
 		_renderer->draw_texture(background_texture, {0,0,256,256}, {0,0,800,600});
-		_renderer->draw_texture(player_texture, 100, 100);
+		_renderer->draw_texture(player_texture, position.x, position.y);
 	}
 	void shutdown() override
 	{
@@ -31,6 +44,9 @@ private:
 	Renderer * _renderer;
 	TextureHandle player_texture;
 	TextureHandle background_texture;
+	
+	Vec2f position;
+	float speed = 200;
 };
 int main()
 {
