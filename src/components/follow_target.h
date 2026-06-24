e792b846e31fbc5baf245dd97a1105d7cf3b9109
IzaltinoDomingosDeSouza <izaltinodsouza@gmail.com>
2026-06-24 19:59:35 -0300

    create a simple follow a target system and components

---
+++ src/components/follow_target.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct FollowTarget
+{
+    entt::entity target;
+    Vec2f offset;
+    bool active;
+};
---
+++ src/systems/follow_target.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "../components/follow_target.h"
+
+void follow_system_update(entt::registry * world)
+{
+	auto view = world->view<FollowTarget, Transform>();
+    for(auto entity : view)
+    {
+		auto & follow = view.get<FollowTarget>(entity);
+		auto & entity_transform = view.get<Transform>(entity);
+		
+		auto * target_transform = world->try_get<Transform>(follow.target);
+		if(follow.active && target_transform)
+		{
+			entity_transform.position.x = target_transform->position.x + follow.offset.x;
+			entity_transform.position.y = target_transform->position.y + follow.offset.y;
+		}
+    }
+}
